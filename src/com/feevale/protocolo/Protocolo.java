fdf4d615ceccc517790090721e958adf77aa7653
= <=>
2016-10-27 02:38:57 -0200

    refatorando

--- src/com/feevale/protocolo/Protocolo.java
+++ src/com/feevale/protocolo/Protocolo.java
@@ -4,14 +4,7 @@ package com.feevale.protocolo;
  * Created by slave00 on 26/10/16.
  */
 public class Protocolo {
-    static final String MENSAGEM_PUCLICA = "$:->mensagem";
-    static final String MENSAGEM_PRIVADA = "$:->privado";
-    static final String SAIU_SALA = "$:->sair";
-    static final String ENTROU_SALA = "$:->entrou";
-    static final String ENVIO_LISTA = "$:->usuario";
-    static final String RECEBE_STATUS = "$:->status";
-
-    public static MensagemCliente parse(String mensagem){
+    public static MensagemCliente parseMensagemClient(String mensagem){
         String comando = mensagem.split(" ")[0];
 
         MensagemCliente msg = MensagemCliente.obterTipoMensagem(comando);
